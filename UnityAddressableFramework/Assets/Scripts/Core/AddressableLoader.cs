87632701390b14d899ec9b84131eab15b82a17d2
fchsg <940299880@qq.com>
2019-10-30 22:04:58 +0800

    code

---
+++ UnityAddressableFramework/Assets/Scripts/Core/AddressableLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Object = UnityEngine.Object;
+
+namespace Core
+{
+    public static class AddressableLoader
+    {
+        public static async Task<T> LoadAssetAsync<T>(string name) where T : Object
+        {
+            T result = null;
+            
+            var handle = Addressables.LoadAssetAsync<T>(name);
+            await handle.Task;
+
+            if (handle.IsDone && handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                result = handle.Result;
+            }
+            else
+            {
+                Debug.LogError($" {name} Load Failed");
+            }
+            
+            return result;
+        }
+
+        public static async Task<AsyncOperationHandle<T>> LoadAssetHandleAsync<T>(string name)
+        {
+            var handle = Addressables.LoadAssetAsync<T>(name);
+            await handle.Task;
+
+            if (handle.Status != AsyncOperationStatus.Succeeded)
+            {
+                Debug.LogError($" {name} Load Failed");
+            }
+
+            return handle;
+        }
+
+        
+
+    }
+}
---
+++ UnityAddressableFramework/Assets/Scripts/SimpleScene.cs
@@ -0,0 +1,22 @@
+﻿using Core;
+using UnityEngine;
+
+public class SimpleScene : MonoBehaviour
+{
+    private const string RootPath =   "Assets/AddressableSimpleData/";
+
+    private async void Start()
+    {
+        var prefab2 = await AddressableLoader.LoadAssetAsync<Object>($"{RootPath}Prefabs/Single/PrefabSprite.prefab");
+        Instantiate(prefab2);
+        
+        var prefab = await AddressableLoader.LoadAssetAsync<Object>($"{RootPath}Prefabs/Single/PrefabCube.prefab");
+        Instantiate(prefab);
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
